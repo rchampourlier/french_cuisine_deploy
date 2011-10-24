7f683878b7c5a9943884be2cfe2ad6266ce5ffff
Romain Champourlier <pro@rchampourlier.com>
2011-10-24 09:40:33 +0200

    Corrected bug

--- lib/recipes/rvm.rb
+++ lib/recipes/rvm.rb
@@ -15,7 +15,7 @@ Capistrano::Configuration.instance.load do
       # If the file /etc/rvmrc contains rvm_trust_rvmrc_flag=0, replace it with rvm_trust_rvmrc_flag=1
       run "sudo sed -e 's/rvm_trust_rvmrc_flag=0/rvm_trust_rvmrc_flag=1/g' /etc/rvmrc > /tmp/etc_rvmrc && sudo mv /tmp/etc_rvmrc /etc/rvmrc"
       # If the file does not contain rvm_trust_rvmrc_flag=, adds it.
-      run "! sudo cat /etc/rvmrc | grep "rvm_trust_rvmrc_flag=" && echo "rvm_trust_rvmrc_flag=1" | sudo tee -a /etc/rvmrc"
+      run "! sudo cat /etc/rvmrc | grep 'rvm_trust_rvmrc_flag=' && echo 'rvm_trust_rvmrc_flag=1' | sudo tee -a /etc/rvmrc"
       # We have to trust both, if we want to use both when performing actions with Capistrano.
     end
   end
