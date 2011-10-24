ff0146407c183a06d2e589b01800283c3d0b6b16
Romain Champourlier <pro@rchampourlier.com>
2011-10-24 09:38:29 +0200

    Updated README. Changed approach for RVM trust rvmrc.

--- lib/recipes/rvm.rb
+++ lib/recipes/rvm.rb
@@ -10,10 +10,12 @@ Capistrano::Configuration.instance.load do
       puts "### rvm gemset create #{(rvm_ruby_string.match(%r%^.*@%).post_match)}"
     end
     
-    desc 'Trust rvmrc file'
-    task :trust_rvmrc do
-      run "rvm rvmrc trust #{current_release}"
-      run "rvm rvmrc trust #{current_path}"
+    desc 'Set /etc/rvmrc parameter to trust all projects\' rvmrc files'
+    task :setup do
+      # If the file /etc/rvmrc contains rvm_trust_rvmrc_flag=0, replace it with rvm_trust_rvmrc_flag=1
+      run "sudo sed -e 's/rvm_trust_rvmrc_flag=0/rvm_trust_rvmrc_flag=1/g' /etc/rvmrc > /tmp/etc_rvmrc && sudo mv /tmp/etc_rvmrc /etc/rvmrc"
+      # If the file does not contain rvm_trust_rvmrc_flag=, adds it.
+      run "! sudo cat /etc/rvmrc | grep "rvm_trust_rvmrc_flag=" && echo "rvm_trust_rvmrc_flag=1" | sudo tee -a /etc/rvmrc"
       # We have to trust both, if we want to use both when performing actions with Capistrano.
     end
   end
